7b2bf7f5a46d2cfdc0b9f971187f01fb61abbe22
Amirreza Azadi <a.azadi2001@gmail.com>
2023-01-23 16:49:00 +0330

    the productReducer component of Redux is complete

---
+++ src/redux/products/productReducer.js
@@ -0,0 +1,33 @@
+const initialState = {
+    loading: false,
+    products: [],
+    error: ""
+}
+
+const productsReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case "FETCH_PRODUCTS_REQUEST":
+            return {
+                ...state,
+                loading: true
+            }
+
+        case "FETCH_PRODUCTS_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                products: payload
+            }
+
+        case "FETCH_PRODUCTS_FAILURE":
+            return {
+                loading: false,
+                error: payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default productsReducer;
