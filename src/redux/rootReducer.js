a721f0e5c91835007f1df89ab35197bdec861209
Amirreza Azadi <a.azadi2001@gmail.com>
2023-01-24 10:14:58 +0330

    the cart components added to the project

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import cartReducer from "./cart/cartReducer";
 import productsReducer from "./products/productReducer";
 
 const rootReducer = combineReducers({
-    productsState: productsReducer
+    productsState: productsReducer,
+    cartState: cartReducer
 })
 
 export default rootReducer; 
